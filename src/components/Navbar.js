31fc72cdaaba20671f918ce92d8dcab2a22b43df
simoneclr <simone.clclr@gmail.com>
2022-04-28 17:10:17 +0200

    Add Dynamic mobile menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Displays a single item from the navigation list
 const NavListItem = ({children, to}) => {
@@ -13,18 +13,32 @@ const NavListItem = ({children, to}) => {
 
 // Displays the Navbar at the top of the page
 function Navbar() {
+
+	// State variable controlling the open/closed navigation menu on mobile
+	const [menuOpen, setMenuOpen] = useState(true)
+
+	// Function that handles click on menu button
+	const onMenuButtonClicked = () => {
+		setMenuOpen(prevState => !prevState)
+	}
+ 
+
 	return (
 		<div className="h-16 px-[5%] lg:px-0 bg-white flex items-center gap-4 lg:gap-8">
-			<button className="shrink-0 lg:hidden">
-				<img src="images/icon-menu.svg" alt="Hamburher menu icon"/>
+			<button className="menu-button z-50 shrink-0 lg:hidden" 
+							data-menu-open={menuOpen} onClick={onMenuButtonClicked}>
+
+				<img className="icon-hamburger h-4 w-4" src="images/icon-menu.svg" alt="Hamburher menu icon"/>
+				<img className="icon-close h-4 w-4" src="images/icon-close.svg" alt="Close menu icon"/>
 			</button>
 
 			<div className="grow lg:grow-0">
 				<img className="pb-1" src="images/logo.svg" alt="Sneakers Brand Logo"/>
 			</div>
 
-			<nav className="hidden lg:block lg:grow">
-				<ul className="flex items-center gap-8">
+			<nav className="fixed inset-0 bg-black/75 lg:static lg:bg-transparent lg:block lg:grow" data-menu-open={menuOpen}>
+				<ul className="bg-white h-full w-64 max-w-full pt-16 pl-[5%] flex flex-col items-start gap-8
+												lg:w-auto lg:p-0 lg:flex-row lg:items-center">
 					<NavListItem>Collections</NavListItem>
 					<NavListItem>Men</NavListItem>
 					<NavListItem>Women</NavListItem>
