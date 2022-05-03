53ce2244dde391436526027041a971e0675fa361
simoneclr <simone.clclr@gmail.com>
2022-05-03 16:55:31 +0200

    Add product information

--- src/views/products/ProductPage.js
+++ src/views/products/ProductPage.js
@@ -4,20 +4,39 @@ import { useSelector } from "react-redux";
 import { selectProductById } from "../../store/products/productsSlice";
 
 import ProductImageGallery from "./ProductImageGallery";
+import ProductPrice from "./ProductPrice";
 
 // Displays a product page
 function ProductPage({productId}) {
 
+	// Select product data from redux store
 	const product = useSelector(state => selectProductById(state, productId))
 
 	return (
-		<div className="flex flex-col lg:flex-row items-stretch lg:justify-between gap-12">
+		<div className="flex flex-col lg:flex-row items-stretch lg:justify-between gap-8 lg:gap-12">
 			<div className="lg:w-1/2 lg:px-12">
 				<ProductImageGallery productId={productId}/>			
 			</div>
 
-			<div className="grid lg:w-1/2 lg:px-12">
-				<h1>{product.name}</h1>
+			<div className="flex flex-col gap-4 px-[5%] lg:w-1/2 lg:px-12">
+				<span className="text-primaryOrange text-sm font-bold uppercase">
+					{product.brand}
+				</span>
+
+				<h1 className="text-3xl font-bold">
+					{product.name}
+				</h1>
+				
+				<p className="text-darkGrayishBlue">
+					{product.description}
+				</p>
+				
+				<ProductPrice initialPrice={product.price} discountFactor={product.discount}/>
+
+				<div>
+					{/* Buttons */}
+				</div>
+
 			</div>
 		</div>
 	)
---
+++ src/views/products/ProductPrice.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+// Givem the initial price and a discount factor, computes and displays the actual price of the product
+const ProductPrice = ({initialPrice, discountFactor}) => {
+
+	const finalPrice = (initialPrice - initialPrice * discountFactor).toFixed(2)
+
+	const isDiscounted = discountFactor > 0
+
+	return (
+		<div className="flex items-center">
+			<div className="flex items-center gap-2 grow">
+				<span className="text-2xl font-bold">
+					<span className="sr-only">Price:</span>
+					${finalPrice}
+				</span>
+
+				{ isDiscounted ? 
+					
+					<span className="text-primaryOrange font-bold bg-paleOrange px-1 rounded-md">
+						<span className="sr-only">Discount:</span>
+						{Math.round(discountFactor * 100)}%
+					</span>
+
+					: ""
+				}
+			</div>
+
+			{ isDiscounted ?
+				
+				<div className="text-grayishBlue font-bold line-through">
+					<span className="sr-only">Original Price:</span>
+					${initialPrice.toFixed(2)}
+				</div>
+				
+				: ""
+			}
+
+		</div>
+	)
+}
+
+export default ProductPrice
