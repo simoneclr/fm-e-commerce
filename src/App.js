831b63602111f1807367d9bf9cfb965bcc144482
simoneclr <simone.clclr@gmail.com>
2022-05-12 15:49:48 +0200

    Refactor container structure

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import ProductPage from './views/products/ProductPage';
 function App() {
   return (
     <div className="App min-h-screen flex flex-col items-stretch">
-      <Navbar/>
+      <div className='grow max-w-[1440px] mx-auto'>
+        <Navbar/>
       
-      <main className="grow pb-8 lg:py-20 lg:px-[10%]">
-        <ProductPage productId={1}/>
-      </main>
+        <main className="pb-8 lg:py-20 lg:px-[10%]">
+          <ProductPage productId={1}/>
+        </main>
+      </div>      
 
       <Footer/>
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,26 +5,30 @@ import { ReactComponent as Logo } from "../assets/svgs/logo.svg"
 // Displays the footer at the bottom of the page
 function Footer() {
 	return (
-		<footer className="bg-veryDarkBlue text-grayishBlue text-sm py-8 px-[5%] lg:px-[10%]">
-			<Logo title="Sneakers brand logo" className="fill-lightGrayishBlue"/>
+		<footer className="bg-veryDarkBlue w-full">
+			<div className="max-w-[1440px] mx-auto">
+				<div className="text-grayishBlue text-sm py-8 px-[5%] lg:px-[10%]">
+					<Logo title="Sneakers brand logo" className="fill-lightGrayishBlue"/>
 
-			<p className="mt-6">
-				<span className="uppercase font-bold text-lightGrayishBlue">
-					Discailmer:
-				</span> This is <span className="uppercase font-bold text-lightGrayishBlue">not</span> a 
-				real e-commerce website. It's only a demo interface built as an excercise
-			</p>
-			
-			<p className="mt-4">
-				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer"
-					className="text-lightGrayishBlue hover:underline">
-					Frontend Mentor
-				</a>.
-				Coded by <a href="https://github.com/simoneclr" target="_blank" rel="noreferrer"
-					className="text-lightGrayishBlue hover:underline">
-					Simone Calciolari
-				</a>.
-			</p>
+					<p className="mt-6">
+						<span className="uppercase font-bold text-lightGrayishBlue">
+							Discailmer:
+						</span> This is <span className="uppercase font-bold text-lightGrayishBlue">not</span> a 
+						real e-commerce website. It's only a demo interface built as an excercise
+					</p>
+					
+					<p className="mt-4">
+						Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer"
+							className="text-lightGrayishBlue hover:underline">
+							Frontend Mentor
+						</a>.
+						Coded by <a href="https://github.com/simoneclr" target="_blank" rel="noreferrer"
+							className="text-lightGrayishBlue hover:underline">
+							Simone Calciolari
+						</a>.
+					</p>
+				</div>
+			</div>
 		</footer>
 	)
 }
