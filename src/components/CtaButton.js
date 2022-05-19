09687e0fc453cc3d539cfba7b841a6c0999574bd
simoneclr <simone.clclr@gmail.com>
2022-05-19 16:56:33 +0200

    Update CtaButton.js to accept type and form attributes

--- src/components/CtaButton.js
+++ src/components/CtaButton.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 // Displays a call to action button
-function CtaButton({children, className, onClick, disabled}) {
+function CtaButton({children, className, type, form, onClick, disabled}) {
 	return (
-		<button onClick={onClick} disabled={disabled}
+		<button onClick={onClick} disabled={disabled} type={type ? type : "button"} form={form ? form : null}
 						className={(className ? className : "") + " " +
 						"bg-primaryOrange text-white font-bold p-3 rounded-lg	hover:opacity-75 disabled:opacity-40"}>
 			{children}
