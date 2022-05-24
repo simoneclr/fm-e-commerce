f6de63e5a4a9bd30dd1ee3cb240337f16c0f7709
simoneclr <simone.clclr@gmail.com>
2022-05-24 15:23:24 +0200

    Prevent navigation to checkout page if the cart is empty

--- src/views/checkout/CheckoutPage.js
+++ src/views/checkout/CheckoutPage.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import { selectTotalCartPrice } from "../../store/cart/cartSlice";
+import { selectAllCartIds, selectTotalCartPrice } from "../../store/cart/cartSlice";
 
 import CartItemsList from "../cart/CartItemsList";
 import CtaButton from "../../components/CtaButton";
 import CheckoutForm from "./CheckoutForm";
+import { Navigate } from "react-router-dom";
 
 // Displays the checkout page
 function CheckoutPage() {
 
+	// Select cart items ids
+	const cartIds = useSelector(selectAllCartIds)
+
+	// Select total price of items in the cart
 	const totalPrice = useSelector(selectTotalCartPrice)
 
 	// TODO: Manage shipping and vat as state variables
@@ -18,6 +23,8 @@ function CheckoutPage() {
 	const vat = 0.25
 
 	return (
+		cartIds.length > 0 ?
+
 		<div className="flex flex-col gap-8 px-[5%] sm:px-[10%] 
 										lg:px-0 lg:flex-row lg:items-start lg:justify-between">
 			<div className="py-4 lg:px-4 lg:grow lg:rounded-xl lg:shadow-xl">
@@ -52,6 +59,11 @@ function CheckoutPage() {
 				</CtaButton>
 			</div>
 		</div>
+
+		:
+
+		// If the cart is empty, prevent visiting this page
+		<Navigate  to="/" replace={true}/>
 	)
 }
 
