d83d9f04a2701d6a20fd3da9f6ef76e75661634c
simoneclr <simone.clclr@gmail.com>
2022-05-26 15:06:18 +0200

    Remove useless prop passed to ProductPage

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage/>} />
             <Route path='products'>
-              <Route path=":productId" element={<ProductPage productId={1}/>}/>
+              <Route path=":productId" element={<ProductPage/>}/>
             </Route>
             <Route path="checkout" element={<CheckoutPage/>}/>            
             <Route path="confirmation" element={<OrderConfirmation/>}/>
