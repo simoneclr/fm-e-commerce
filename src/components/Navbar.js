5a858865f9474088791d5927c2ec6aee7a8f86ba
simoneclr <simone.clclr@gmail.com>
2022-05-31 16:33:42 +0200

    Fix link warning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,8 +11,8 @@ import MobileMenuButton from "./MobileMenuButton";
 const NavListItem = ({children, to, onClick}) => {
 	return (
 		<li>
-			<a href={to ? to : "javascript:void(0)"} onClick={onClick} className="
-				text-veryDarkBlue font-bold lg:text-darkGrayishBlue lg:hover:text-veryDarkBlue lg:font-normal
+			<a href={to ? to : null} onClick={onClick} className="
+				text-veryDarkBlue cursor-pointer font-bold lg:text-darkGrayishBlue lg:hover:text-veryDarkBlue lg:font-normal
 				lg:relative lg:h-full lg:flex lg:items-center
 				lg:after:absolute lg:after:bottom-0 lg:after:w-full lg:after:h-1 lg:after:bg-primaryOrange
 				lg:after:scale-x-0 lg:hover:after:scale-x-100 lg:after:transition-transform lg:after:duration-500">
